a634e6a6ae29e8f12a49911767438ecb0b056690
sashkaochigava <sashkaochi@gmail.com>
2024-06-06 16:47:00 +0400

    feat: created task container component

---
+++ src/components/TaskContainer.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  time: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="bg-white box-shadow-all-sides flex flex-col items-center rounded-lg"
+  >
+    <div class="flex items-center">
+      <input type="checkbox" />
+      <h1>{{ title }}</h1>
+    </div>
+    <h1>{{ time }}</h1>
+  </div>
+</template>
