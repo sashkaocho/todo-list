223807480971c11845817f3cd43ec86d8414d099
sashkaochigava <sashkaochi@gmail.com>
2024-06-06 16:52:18 +0400

    feat: created view for tasks page and used task container

---
+++ src/views/TaskView.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import TaskContainer from "../components/TaskContainer.vue";
+
+const tasks = [
+  { id: 1, title: "Clean house", time: "12:00" },
+  { id: 1, title: "Workout", time: "13:00" },
+  {
+    id: 1,
+    title: "Eat",
+    time: "11:00",
+  },
+  { id: 1, title: "Read Book", time: "15:00" },
+];
+</script>
+
+<template>
+  <h1 class="text-xl">Hello, Aleksandre 👋</h1>
+  <section class="grid grid-cols-3 gap-y-4 gap-x-4 h-[350px]">
+    <TaskContainer
+      v-for="task in tasks"
+      :key="task.title + task.time"
+      :time="task.time"
+      :title="task.title"
+    />
+  </section>
+</template>
