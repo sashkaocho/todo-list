e9c291351df96443eaa4e8b7ce8941c3f8eae5e5
sashkaochigava <sashkaochi@gmail.com>
2024-06-07 21:43:08 +0400

    feat: added description to tasks

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -2,7 +2,13 @@
 import TaskContainer from "../components/TaskContainer.vue";
 
 const tasks = [
-  { id: 1, title: "Clean house", time: "12:00" },
+  {
+    id: 1,
+    title: "Clean house",
+    time: "12:00",
+    description:
+      "Clean house and manage that all tasks is done so we are gonna make pilesos on and start cleaning",
+  },
   { id: 1, title: "Workout", time: "13:00" },
   {
     id: 1,
@@ -14,13 +20,20 @@ const tasks = [
 </script>
 
 <template>
-  <h1 class="text-xl">Hello, Aleksandre 👋</h1>
-  <section class="grid grid-cols-3 gap-y-4 gap-x-4 h-[350px]">
-    <TaskContainer
-      v-for="task in tasks"
-      :key="task.title + task.time"
-      :time="task.time"
-      :title="task.title"
-    />
-  </section>
+  <article class="flex flex-col gap-10">
+    <section>
+      <h1 class="text-xl">Hello, I'm your todo manager 👋</h1>
+      <h1>today</h1>
+    </section>
+
+    <section class="grid grid-cols-3 gap-y-4 gap-x-4">
+      <TaskContainer
+        v-for="task in tasks"
+        :key="task.title + task.time"
+        :description="task.description"
+        :time="task.time"
+        :title="task.title"
+      />
+    </section>
+  </article>
 </template>
