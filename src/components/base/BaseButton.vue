1a33ef24a4b5664e1086b3c9ede85c4b23b13a37
sashkaochigava <sashkaochi@gmail.com>
2024-06-07 21:42:27 +0400

    feat: created BaseButton component

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  width: number;
+  height: number;
+}>();
+</script>
+
+<template>
+  <button
+    :style="`width: ${width}px; height: ${height}px`"
+    class="rounded-xl text-primary-700 border border-primary-700 transition-all hover:bg-primary-700 hover:text-white"
+  >
+    {{ title }}
+  </button>
+</template>
+<!--py-2 px-4-->
