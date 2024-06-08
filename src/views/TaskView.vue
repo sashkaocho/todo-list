eafa7e45291c9c1f2cc4d5ca6612d6de293e2f1e
sashkaochigava <sashkaochi@gmail.com>
2024-06-08 16:19:43 +0400

    chore: imported TaskDetailsDialog

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
 import TaskContainer from "../components/TaskContainer.vue";
+import BaseSelect from "../components/base/BaseSelect.vue";
+import { ref } from "vue";
+import { useTaskStore } from "../pinia/task.pinia.ts";
+import TaskDetailsDialog from "../components/TaskDetailsDialog.vue";
+
+const taskStore = useTaskStore();
 
 const tasks = [
   {
     id: 1,
-    title: "Clean house",
+    title: "Clean house and make all sd",
     time: "12:00",
     status: "to do",
     description:
-      "Clean house and manage that all tasks is done so we are gonna make pilesos on and start cleaning",
+      "Clean house and manage that all tasks is done so we are gonna make pilesos on and start cleaning also idiot to make sense asasdfasdfa sdf sdfsd",
   },
   { id: 1, title: "Workout", time: "13:00", status: "done" },
   {
@@ -19,14 +25,25 @@ const tasks = [
   },
   { id: 1, title: "Read Book", time: "15:00", status: "failed" },
 ];
+
+const filterItems = ["All", "To do", "Done", "Failed"];
+
+const selectedTask = ref(null);
+
+const selectTask = (task) => {
+  taskStore.taskDetailsDialog = true;
+  selectedTask.value = task;
+};
 </script>
 
 <template>
   <article class="flex flex-col gap-10">
-    <section>
-      <h1 class="text-xl">Hello, I'm your todo manager 👋</h1>
-      <h1>today</h1>
+    <section class="flex items-center justify-between">
+      <h2>Hello, I'm your todo manager 👋</h2>
+      <BaseSelect :items="filterItems" :width="150" />
     </section>
+
+    <TaskDetailsDialog :task="selectedTask" />
 
     <section class="grid grid-cols-3 gap-y-4 gap-x-4">
       <TaskContainer
@@ -36,6 +53,7 @@ const tasks = [
         :status="task.status"
         :time="task.time"
         :title="task.title"
+        @click="selectTask(task)"
       />
     </section>
   </article>
