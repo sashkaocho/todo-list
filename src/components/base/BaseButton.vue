29b12293f50cc62b2494b04a4cbc7d2ecd204f99
sashkaochigava <sashkaochi@gmail.com>
2024-06-08 16:08:11 +0400

    feat: added dynamic color to BaseButton

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,17 +1,31 @@
 <script lang="ts" setup>
-defineProps<{
+import { computed, ref } from "vue";
+
+const props = defineProps<{
   title: string;
   width: number;
   height: number;
+  color: string;
 }>();
+
+const isHovered = ref<boolean>(false);
+
+const buttonStyle = computed(() => ({
+  width: `${props.width}px`,
+  height: `${props.height}px`,
+  color: isHovered.value ? "white" : props.color,
+  border: `1px solid ${props.color}`,
+  backgroundColor: isHovered.value ? props.color : "transparent",
+}));
 </script>
 
 <template>
   <button
-    :style="`width: ${width}px; height: ${height}px`"
-    class="rounded-xl text-primary-700 border border-primary-700 transition-all hover:bg-primary-700 hover:text-white"
+    :style="buttonStyle"
+    class="rounded-xl border transition-all hover:text-white"
+    @mouseleave="isHovered = false"
+    @mouseover="isHovered = true"
   >
     {{ title }}
   </button>
 </template>
-<!--py-2 px-4-->
