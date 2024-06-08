5bc13beed3bdf366c15a882cf94145fd1771308c
sashkaochigava <sashkaochi@gmail.com>
2024-06-08 16:16:27 +0400

    style: used configured font styles

--- src/components/TaskContainer.vue
+++ src/components/TaskContainer.vue
@@ -22,18 +22,18 @@ const statusColor = computed(() => {
 
 <template>
   <div
-    class="bg-white box-shadow-all-sides flex flex-col justify-between rounded-lg py-3 px-5 h-44 cursor-pointer"
+    class="bg-white box-shadow-all-sides-hovered flex flex-col justify-between rounded-lg py-3 px-5 h-44 cursor-pointer"
   >
     <div class="w-full flex items-center justify-between">
-      <h1 class="text-2xl">{{ title }}</h1>
+      <h1>{{ title }}</h1>
       <input type="checkbox" />
     </div>
-    <p class="text-sm">{{ description }}</p>
+    <p>{{ description }}</p>
     <div class="flex justify-between">
-      <h2 class="text-lg">{{ time }}</h2>
-      <h2 :style="`color: ${statusColor}`" class="text-primary-700">
+      <h3>{{ time }}</h3>
+      <h4 :style="`color: ${statusColor}`" class="text-primary-700">
         {{ status }}
-      </h2>
+      </h4>
     </div>
   </div>
 </template>
