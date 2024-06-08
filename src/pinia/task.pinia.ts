27b88f0c68d1cddf37cee55b9a3806aea1ccd99d
sashkaochigava <sashkaochi@gmail.com>
2024-06-08 16:10:06 +0400

    feat: created interface for task state

--- src/pinia/task.pinia.ts
+++ src/pinia/task.pinia.ts
@@ -1,7 +1,8 @@
 import { defineStore } from "pinia";
+import { ITaskState } from "../ts/interfaces/pinia/task.interface.ts";
 
 export const useTaskStore = defineStore("task", {
-  state: () => ({
+  state: (): ITaskState => ({
     createTaskDialog: false,
     taskDetailsDialog: false,
   }),
