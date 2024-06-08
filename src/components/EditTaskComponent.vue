c31e8dd7187d32a952833094a385a3764327f20d
sashkaochigava <sashkaochi@gmail.com>
2024-06-08 16:19:09 +0400

    feat: created EditTask component

---
+++ src/components/EditTaskComponent.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { EColor } from "../ts/enums/color.enum.ts";
+import BaseInput from "./base/BaseInput.vue";
+import BaseButton from "./base/BaseButton.vue";
+
+defineEmits(["cancel"]);
+</script>
+
+<template>
+  <section class="flex flex-col">
+    <form class="flex flex-col gap-6">
+      <BaseInput label="Name" type="text" />
+      <BaseInput label="Description" type="text" />
+      <BaseInput label="Start time" type="datetime-local" />
+      <BaseInput label="End time" type="datetime-local" />
+      <div class="flex justify-between items-center mt-2">
+        <BaseButton
+          :color="EColor.success"
+          :height="40"
+          :width="100"
+          title="Save"
+        />
+        <BaseButton
+          :color="EColor.error"
+          :height="40"
+          :width="100"
+          title="Cancel"
+          @click="$emit('cancel')"
+        />
+      </div>
+    </form>
+  </section>
+</template>
