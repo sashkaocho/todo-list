4579e0cf2d2600da999ea3e60c708ccd6b471600
sashkaochigava <sashkaochi@gmail.com>
2024-06-09 17:41:27 +0400

    feat: added label for max characters

--- src/components/base/BaseTextField.vue
+++ src/components/base/BaseTextField.vue
@@ -11,7 +11,12 @@ const model = defineModel<string>();
 
 <template>
   <div class="flex flex-col gap-1.5">
-    <label class="text-sm text-primary-700">{{ label }}</label>
+    <div class="flex items-center justify-between">
+      <label class="text-sm text-primary-700">{{ label }} </label>
+      <p v-if="maxlength" class="text-warning text-xs ml-2">
+        Max {{ maxlength }} characters*
+      </p>
+    </div>
     <input
       v-if="field === 'input'"
       v-model="model"
