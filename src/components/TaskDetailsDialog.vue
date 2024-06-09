cdefb6fc6a3c0d1cec65ddffec8435a3965db78b
sashkaochigava <sashkaochi@gmail.com>
2024-06-10 00:28:00 +0400

    chore: used format date function

--- src/components/TaskDetailsDialog.vue
+++ src/components/TaskDetailsDialog.vue
@@ -8,6 +8,7 @@ import EditTaskComponent from "./EditTaskComponent.vue";
 import { ITask } from "../ts/interfaces/task.interface.ts";
 import axios from "axios";
 import { getTasks } from "../utils/task.ts";
+import { formatDate } from "../utils/helpers/date.ts";
 
 const props = defineProps<{
   task: ITask | null;
@@ -32,6 +33,25 @@ const deleteTask = async (): Promise<void> => {
   }
 };
 
+const formattedTaskTime = computed<string>(() => {
+  return (
+    formatDate(props.task?.start_date as string) +
+    " - " +
+    formatDate(props.task?.end_date as string)
+  );
+});
+
+const statusColor = computed(() => {
+  switch (props.task?.status) {
+    case "todo":
+      return "#3e3ac8";
+    case "done":
+      return "#16a34a";
+    case "failed":
+      return "#dc2626";
+  }
+});
+
 const closeTaskDetails = (): void => {
   taskStore.taskDetailsDialog = false;
   isEditing.value = false;
@@ -49,14 +69,21 @@ const startEditing = (): void => {
     :width="450"
     @close="closeTaskDetails"
   >
-    <EditTaskComponent v-if="isEditing" @cancel="isEditing = false" />
+    <EditTaskComponent
+      v-if="isEditing"
+      :task="task"
+      @cancel="isEditing = false"
+      @close="closeTaskDetails"
+    />
+
     <section v-else class="flex flex-col gap-4">
-      <h3 class="text-primary-700">{{ task.name }}</h3>
-      <p>{{ task.description }}</p>
-      <h3>{{ task.start_date }}</h3>
-      <h4>{{ task.status }}</h4>
+      <h3 class="text-primary-700">{{ task?.name }}</h3>
+      <p>{{ task?.description }}</p>
+      <h3>{{ formattedTaskTime }}</h3>
+      <h4 :style="`color: ${statusColor}`">{{ task?.status }}</h4>
       <div class="flex items-center justify-between mt-2">
         <BaseButton
+          v-if="task?.status === 'todo'"
           :color="EColor.warning"
           :height="40"
           :width="100"
