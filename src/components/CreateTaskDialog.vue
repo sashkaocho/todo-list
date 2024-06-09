e75ab88680b86d882b6c6b7d5b426bf4295f71f0
sashkaochigava <sashkaochi@gmail.com>
2024-06-10 00:30:41 +0400

    refactor: refactored CreateTaskDialog

--- src/components/CreateTaskDialog.vue
+++ src/components/CreateTaskDialog.vue
@@ -5,25 +5,27 @@ import BaseTextField from "./base/BaseTextField.vue";
 
 import { useTaskStore } from "../pinia/task.pinia.ts";
 import { EColor } from "../ts/enums/color.enum.ts";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import axios from "axios";
 import { getTasks } from "../utils/task.ts";
 
 const taskStore = useTaskStore();
 
-const nameModel = ref<string>("");
-const descriptionModel = ref<string>("");
-const startDateModel = ref<string>("");
-const endDateModel = ref<string>("");
+const formData = reactive({
+  nameModel: "",
+  descriptionModel: "",
+  startDateModel: "",
+  endDateModel: "",
+});
 
 const formIsValid = ref<boolean>(false);
 
 const validateForm = (): void => {
   if (
-    nameModel.value.length === 0 ||
-    descriptionModel.value.length === 0 ||
-    startDateModel.value.length === 0 ||
-    endDateModel.value.length === 0
+    formData.nameModel.length === 0 ||
+    formData.descriptionModel.length === 0 ||
+    formData.startDateModel.length === 0 ||
+    formData.endDateModel.length === 0
   ) {
     formIsValid.value = false;
     return;
@@ -37,10 +39,10 @@ const createTask = async (): Promise<void> => {
   if (!formIsValid.value) return;
   try {
     const newTask = {
-      name: nameModel.value,
-      description: descriptionModel.value,
-      start_date: startDateModel.value,
-      end_date: endDateModel.value,
+      name: formData.nameModel,
+      description: formData.descriptionModel,
+      start_date: formData.startDateModel,
+      end_date: formData.endDateModel,
     };
 
     await axios.post("http://127.0.0.1:8000/api/tasks", newTask);
@@ -54,10 +56,10 @@ const createTask = async (): Promise<void> => {
 };
 
 const emptyFields = (): void => {
-  nameModel.value = "";
-  descriptionModel.value = "";
-  startDateModel.value = "";
-  endDateModel.value = "";
+  formData.nameModel = "";
+  formData.descriptionModel = "";
+  formData.startDateModel = "";
+  formData.endDateModel = "";
 };
 
 const closeCreateTaskDialog = (): void => {
@@ -75,27 +77,27 @@ const closeCreateTaskDialog = (): void => {
     <div>
       <form class="flex flex-col gap-7" @submit.prevent="createTask">
         <BaseTextField
-          v-model="nameModel"
+          v-model="formData.nameModel"
           :maxlength="24"
           field="input"
           label="Name"
           type="text"
         />
         <BaseTextField
-          v-model="descriptionModel"
+          v-model="formData.descriptionModel"
           :maxlength="135"
           field="textarea"
           label="Description"
           type="text"
         />
         <BaseTextField
-          v-model="startDateModel"
+          v-model="formData.startDateModel"
           field="input"
           label="Start date"
           type="datetime-local"
         />
         <BaseTextField
-          v-model="endDateModel"
+          v-model="formData.endDateModel"
           field="input"
           label="End date"
           type="datetime-local"
